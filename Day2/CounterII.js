fe87fdd36c700aff274ccd4d2ed743da2fed1ac8
salah <salahuznr@gmail.com>
2024-02-21 19:58:13 +0530

    counter

--- Day2/CounterII.js
+++ Day2/CounterII.js
@@ -38,3 +38,18 @@
 // <=====================================  answer  ========================>
 
 
+var createCounter = function(init) {
+    let num = init;
+ return {
+     increment: () => num = num + 1,
+     reset: () => num = init,
+     decrement: () => num = num - 1
+ };
+ 
+};
+
+
+ const counter = createCounter(5)
+ counter.increment(); // 6
+ counter.reset(); // 5
+ counter.decrement(); // 4
