882611029708e628c159cfbba97cf29ddad8a098
salah <salahuznr@gmail.com>
2024-02-22 19:56:28 +0530

    solution

--- Day3/transformEachElemntInArray.js
+++ Day3/transformEachElemntInArray.js
@@ -24,3 +24,34 @@
 // Input: arr = [10,20,30], fn = function constant() { return 42; }
 // Output: [42,42,42]
 // Explanation: The function always returns 42.
+
+
+
+// <=====================================  answer  ========================>
+
+function map(arr, fn) {
+    return arr.map((element, index) => fn(element, index));
+  }
+
+//   example 1
+const arr1 = [1, 2, 3];
+const plusone = function plusone(n) { return n + 1; }
+
+const newArray1 = map(arr1, plusone);
+console.log(newArray1); // Output: [2, 3, 4]
+
+// example 2
+const arr2 = [1, 2, 3];
+const plusI = function plusI(n, i) { return n + i; }
+
+const newArray2 = map(arr2, plusI);
+console.log(newArray2); // Output: [1, 3, 5]
+
+
+// example 3
+const arr3 = [10, 20, 30];
+const constant = function constant() { return 42; }
+
+const newArray3 = map(arr3, constant);
+console.log(newArray3); // Output: [42, 42, 42]
+
