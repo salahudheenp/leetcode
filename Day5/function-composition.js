3407cd9a49b056b84e8a7def7f9226be754bdc01
salah <salahuznr@gmail.com>
2024-03-02 13:55:36 +0530

    fial

--- Day5/function-composition.js
+++ Day5/function-composition.js
@@ -39,7 +39,15 @@
 
 var compose = function(functions) {
     
-    return function(x) {
-        
+	return function(x) {
+        for(let i=functions.length-1;i>=0;i--){
+            x=functions[i](x)
+        }
+        return x
     }
 };
+
+
+  const fn = compose([x => x + 1, x => 2 * x])
+  fn(4) // 9
+ 
